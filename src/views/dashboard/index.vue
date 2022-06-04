4b1a342c0aa481c38d14e95ecdad2f4cd0875f13
你的微笑暖暖的 <1245488569@qq.com>
2022-06-04 23:42:02 +0800

    feat: elementui升级

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,18 +1,12 @@
 <script setup lang="ts" name="dashboard">
-  const num = ref(1)
+  import { ElMessage } from 'element-plus'
+  const showMsg = () => {
+    ElMessage('this is a message.')
+  }
 </script>
 
 <template>
   <page-main>
-    <i-ep-arrow-right />
-    <i-ep-baseball />
-    <!-- <i-ep-map-location /> -->
-    <SvgIcon name="ep:map-location" />
-
-    <div>
-      <el-input-number v-model="num" :min="1" :max="10" />
-    </div>
-
-    <div v-for="i in 100" :key="i">{{ i }}</div>
+    <el-button type="primary" @click="showMsg">Primary</el-button>
   </page-main>
 </template>

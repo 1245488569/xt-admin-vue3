bf5d95b23c5f0a05c75df3a18df4e4c8298ceff7
你的微笑暖暖的 <1245488569@qq.com>
2023-09-14 20:29:18 +0800

    feat: 打包配置

--- vite.config.ts
+++ vite.config.ts
@@ -12,5 +12,36 @@ export default defineConfig(({ command, mode }) => {
     resolve: {
       alias,
     },
+    base: './',
+    server: {
+      proxy: {
+        '/proxy': {
+          target: env.VITE_APP_API_BASEURL,
+          changeOrigin: command === 'serve' && env.VITE_OPEN_PROXY === 'true',
+          rewrite: path => path.replace(/\/proxy/, ''),
+        },
+      },
+    },
+    build: {
+      outDir: mode === 'production' ? 'dist' : `dist-${mode}`,
+      assetsDir: 'assets',
+      minify: 'terser',
+      sourcemap: env.VITE_BUILD_SOURCEMAP === 'true',
+      terserOptions: {
+        compress: {
+          keep_infinity: true,
+          drop_console: env.VITE_BUILD_DROP_CONSOLE === 'true',
+          drop_debugger: env.VITE_BUILD_DROP_DEBUGGER === 'true',
+        },
+      },
+      chunkSizeWarningLimit: 1500,
+      rollupOptions: {
+        output: {
+          chunkFileNames: 'assets/js/[name]-[hash].js',
+          entryFileNames: 'assets/js/[name]-[hash].js',
+          assetFileNames: 'assets/[ext]/[name]-[hash].[ext]',
+        },
+      },
+    },
   }
 })

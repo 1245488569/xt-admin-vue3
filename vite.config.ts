a2d11b41205ce614018323e7595827c09da1263e
你的微笑暖暖的 <1245488569@qq.com>
2023-09-14 22:44:27 +0800

    fix: config修改

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,6 @@ import { defineConfig, loadEnv } from 'vite'
 import alias from './vite/alias'
 import setupVitePlugins from './vite/plugins'
 
-// eslint-disable-next-line no-control-regex
-const INVALID_CHAR_REGEX = /[\u0000-\u001F"#$&*+,:;<=>?[\]^`{|}\u007F]/g
-const DRIVE_LETTER_REGEX = /^[a-z]:/i
-
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, process.cwd())
@@ -44,16 +40,6 @@ export default defineConfig(({ command, mode }) => {
           chunkFileNames: 'assets/js/[name]-[hash].js',
           entryFileNames: 'assets/js/[name]-[hash].js',
           assetFileNames: 'assets/[ext]/[name]-[hash].[ext]',
-
-          // 解决无法再github pages上访问的问题
-          sanitizeFileName(name) {
-            const match = DRIVE_LETTER_REGEX.exec(name)
-            const driveLetter = match ? match[0] : ''
-            return (
-              driveLetter
-              + name.slice(driveLetter.length).replace(INVALID_CHAR_REGEX, '')
-            )
-          },
         },
       },
     },

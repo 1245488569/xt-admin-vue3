f4ccd8b3ecd4058a44d4eaaf2b91fb03fcfe9d4f
liuxincai <liuxincai>
2023-09-19 17:59:18 +0800

    feat: 添加打包压缩配置

--- vite/plugins/index.ts
+++ vite/plugins/index.ts
@@ -7,15 +7,21 @@ import setupComponents from './components'
 import setupSvgIcon from './svg-icon'
 import setupIcons from './icon'
 import setupMock from './mock'
+import setupCompression from './compression'
 
-export default function setupVitePlugins(viteEnv: Record<string, string>, isBulid: boolean) {
-  const { VITE_USE_MOCK } = viteEnv
+export default function setupVitePlugins(viteEnv: Record<string, string>, isBuild: boolean) {
+  const { VITE_USE_MOCK, VITE_BUILD_COMPRESS } = viteEnv
 
   const plugins: PluginOption[] = [vue(), vueJsx(), UnoCSS()]
   plugins.push(setupAutoImport())
   plugins.push(setupComponents())
   plugins.push(setupIcons())
-  plugins.push(setupSvgIcon(isBulid))
-  VITE_USE_MOCK === 'true' && plugins.push(setupMock(isBulid))
+  plugins.push(setupSvgIcon(isBuild))
+  if (isBuild) {
+    const compressList = VITE_BUILD_COMPRESS.split(',')
+    if (compressList.includes('gzip') || compressList.includes('brotli'))
+      plugins.push(...setupCompression(viteEnv))
+  }
+  VITE_USE_MOCK === 'true' && plugins.push(setupMock(isBuild))
   return plugins
 }

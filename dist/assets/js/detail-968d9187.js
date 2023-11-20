ff2e97943612a3fb84d23fd3610303e6d6642838
你的微笑暖暖的 <1245488569@qq.com>
2023-11-20 21:25:44 +0800

    fix: 打包

--- dist/assets/js/detail-968d9187.js
+++
@@ -1,1 +0,0 @@
-import{_ as s}from"./index-e6add52d.js";import{d as t,b as e,o as a,k as o,w as r,i,t as n,j as d}from"./index-fa77e1b2.js";import"./index-0f552af3.js";import"./plugin-vueexport-helper-1b428a4d.js";const p=t({__name:"detail",setup(t){const p=e().query.id;return(t,e)=>{const u=s;return a(),o(u,null,{default:r((()=>[i(" goods_detail + "+n(d(p)),1)])),_:1})}}});export{p as default};
---
+++ dist/assets/js/detail-c90022e0.js
@@ -0,0 +1,1 @@
+import{_ as s}from"./index-10692e69.js";import{d as t,b as e,o as a,k as o,w as r,i,t as n,j as d}from"./index-9ed729b3.js";import"./index-f3ab3ec8.js";import"./plugin-vueexport-helper-1b428a4d.js";const p=t({__name:"detail",setup(t){const p=e().query.id;return(t,e)=>{const u=s;return a(),o(u,null,{default:r((()=>[i(" goods_detail + "+n(d(p)),1)])),_:1})}}});export{p as default};

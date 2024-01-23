006f5c02982fc3dfa4fdaf4074096e02a6404a23
刘鑫材 <1245488569@qq.com>
2024-01-23 15:29:59 +0800

    fix: eslint配置修改

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,11 @@
 import antfu from '@antfu/eslint-config'
 
 export default antfu({
+  unocss: true,
+  ignores: [
+    'public',
+    'dist*',
+  ],
   rules: {
     'no-console': 'off',
     'node/prefer-global/process': 'off',

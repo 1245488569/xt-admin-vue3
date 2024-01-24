148ce7fcd1373a80ffdc3121b35d043b0acdcf64
刘鑫材 <1245488569@qq.com>
2024-01-24 11:45:48 +0800

    fix: 代码格式化

--- types/env.d.ts
+++ types/env.d.ts
@@ -4,7 +4,6 @@ declare module 'element-plus/dist/locale/zh-cn.mjs'
 declare module 'element-plus/dist/locale/zh-tw.mjs'
 declare module 'element-plus/dist/locale/en.mjs'
 
-
 interface ImportMetaEnv {
   VITE_MODE_NAME: string
   VITE_APP_TITLE: string
@@ -12,8 +11,8 @@ interface ImportMetaEnv {
   VITE_OPEN_PROXY: string
   VITE_USE_MOCK: string
   VITE_BUILD_SOURCEMAP: string
-  VITE_BUILD_DROP_CONSOLE:string
-  VITE_BUILD_DROP_DEBUGGER:string
+  VITE_BUILD_DROP_CONSOLE: string
+  VITE_BUILD_DROP_DEBUGGER: string
 }
 
 interface ImportMeta {

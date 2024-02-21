18e2ff5542033ed04e961ecf368cbfa86eb91494
刘鑫材 <1245488569@qq.com>
2024-02-21 13:47:23 +0800

    fix: 打包

--- dist/assets/js/reload-t0kn5INe.js
+++
@@ -1,1 +0,0 @@
-import{d as s,c as a,G as o,o as e,g as t}from"./index-3dI-2cKY.js";const r=s({__name:"reload",setup(s){const r=a();return o((()=>{r.go(-1)})),(s,a)=>(e(),t("div"))}});export{r as default};
---
+++ dist/assets/js/useLocalI18n-s9m8QoLj.js
@@ -0,0 +1,1 @@
+import{X as t,u as n,z as e}from"./index-p9v_E7Wc.js";function a(){return{generateTitle:function(n){if(!n)return"";const e=t();return e.te(n)?e.t(n):n},watchSwitchLang:function(...t){const a=n();e((()=>a.getLanguage),(()=>{t.forEach((t=>t(a.getLanguage)))}))}}}export{a as u};
